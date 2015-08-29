6452a617272b4c9cf478f23fd5e29f0e515e27b4
Siddharth Ram <siddharth_ram@intuit.com>
2015-08-29 16:53:47 -0700

    changed to imgur and reddit.com/r/quotes

--- routes/index.js
+++ routes/index.js
@@ -2,93 +2,81 @@ var express = require('express');
 var router = express.Router();
 var PropertyReader  = require('properties-reader');
 var properties      = PropertyReader("./properties");
-
 var console = require('console');
 var request = require('request');
-console.log("key is", properties.get("flickr-key"));
-
-
-var flickr = require('flickrapi');
-var qod;
-var auth;
-var photo_url;
-
-    flickrOptions = {
-        api_key: properties.get("flickr-key"),
-        secret: properties.get("flickr-secret")
-    };
-
+var imgur_url = 'https://api.imgur.com/3/gallery/search/\?q\="earthporn"';
+var reddit_url = 'http://api.reddit.com/r/quotes/hot.json?perPage=1'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    flickr.tokenOnly(flickrOptions, function (err, f) {
-        getPic("landscape", f, function (res) {
-            res.render('index', {photo_url: res.photo_url, quote: res.quote, author: res.auth});
+        getPic("landscape", function (ret) {
+            res.render('index', {photo_url: ret.photo_url, quote: ret.quote});
         });
-    });
 });
 
 
 
-function getPic(theme,flickr, callback) {
-    // if (flickr == null) {
-    // flickr = new flickrapi.Flickr(properties.get("flickr-key"));
-    //}
-    flickr.photos.search({
-            text: "landscape",
-            per_page: 1,
-            extras: "url_l"
-        },
-        function (err, result) {
-            if (err) {
-                throw new Error(err);
-            } else {
-                console.log('photo json is', JSON.stringify(result));
+function getPic(theme, callback) {
+    var imgur_key =  properties.get('imgur-client');
+    var auth = 'Client-ID ' + imgur_key;
+    //var agent =  "NodeJS:" + auth + ":<v.1>" + "\(by \/u\/\<srram\>\)";
+    var agent =  "NodeJS:v.1 by /u/srram";
+    console.log ("auth is....", auth);
+    var imgur_options = {
+        url: imgur_url,
+        headers: {
+            Authorization: auth
+        }
+    };
+    var reddit_options = {
+        url: reddit_url,
+        headers: {
+            'User-Agent': agent
+        }
+    };
+    var quote;
+    var photo;
+    request(imgur_options, function(i_err, i_response, i_body) {
+        if (!i_err && i_response.statusCode == 200) {
+            var i_info = JSON.parse(i_body);
+           // console.log('==info-==', i_info);
+            console.log(i_info.data[0].link);
+            photo = i_info.data[0].link;
+            // get the reddit quote
+            request(reddit_options, function (r_err, r_response, r_body) {
 
-                //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
-                /*
-                 var url = "https://farm" +
-                 result.photos.photo[0].farm   +
-                 ".staticflickr.com"  + "/" +
-                 result.photos.photo[0].server + "/" +
-                 result.photos.photo[0].id +
-                 "_" +
-                 result.photos.photo[0].secret +
-                 "_h" +
-                 ".png";
-                 */
-                var qodoptions = {
-                    host: 'api.theysaidso.com',
-                    path: '/qod?category=inspire',
-                };
-                if (qod == null) {
-                    request('http://api.theysaidso.com/qod.json?category=inspire', function (err, resp) {
-                        if (err) {
-                            console.log("theysaidsoerror:", err);
-                        } else {
-                            console.log("resp is", JSON.stringify(resp));
-                            qod = resp.body.contents.quotes[0].quote;
-                            auth = resp.contents.quotes[0].author;
-                            var res = {
-                                photo_url: result.photos.photo[0].url_l,
-                                quote: qod,
-                                author: auth
-                            };
-                            callback(res);
-                        }
-                    });
+                var r_info = JSON.parse(r_body);
+                console.log ("body is ", JSON.stringify(r_info));
+
+                if (!r_err && r_response.statusCode == 200) {
+                    //console.log ("==reddit:====",r_info );
+                    var offset = random (1, r_info.data.children.length);
+                    console.log ("offset is" + offset);
+                   // if (r_info.data.children.length > 1) {
+                     //   offset = 1;
+                    //}
+                    quote = r_info.data.children[offset].data.title;
+                    console.log ("qbote is..", quote);
+                    var ret = {
+                        photo_url: photo,
+                        quote: quote
+                    }
+                callback(ret);
                 } else {
-                    var res = {
-                        photo_url: result.photos.photo[0].url_l,
-                        quote: qod,
-                        author: auth
-
-                    };
-                    callback(res);
+                    console.log("reddit error", r_response.statusCode);
                 }
-            }
-        })
+            });
+        } else {
+            console.log("errored out",i_response, i_response.statusCode);
+        }
+    });
 }
+
+function random (low, high) {
+    return Math.round(Math.random() * (high - low) + low);
+}
+
+
 
 
 module.exports = router;
